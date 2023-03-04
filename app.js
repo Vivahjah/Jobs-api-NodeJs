bdeb5231f486dfc16acf1b396efa37ea917ea85a
Mengwa Ugo <mengwa44@yahoo.com>
2023-03-04 04:38:28 +0100

    added user to DB

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
+const authRouter = require('./routes/auth')
+const jobsRouter = require('./routes/jobs')
 const app = express();
 
-// error handler
+//connect DB
+const connectDB = require('./db/connect')
+    // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
@@ -11,10 +15,9 @@ app.use(express.json());
 // extra packages
 
 // routes
-app.get('/', (req, res) => {
-    res.send('jobs api');
-});
 
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/jobs', jobsRouter)
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
@@ -22,6 +25,7 @@ const port = process.env.PORT || 5000;
 
 const start = async() => {
     try {
+        await connectDB(process.env.MONGO_URI)
         app.listen(port, () =>
             console.log(`Server is listening on port ${port}...`)
         );
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,19 @@
-const register = (req, res) => {
-    res.send('Register User')
+const User = require('../models/User')
+const { StatusCodes } = require('http-status-codes')
+const { BadRequestError } = require('../errors')
+const bcrypt = require('bcryptjs')
+
+
+const register = async(req, res) => {
+    const { name, password, email } = req.body;
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const tempUser = { name, email, password: hashedPassword }
+
+    const user = await User.create({...tempUser })
+    res.status(StatusCodes.CREATED).json({ user })
 }
 
 const login = (req, res) => {
