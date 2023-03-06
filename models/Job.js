519c6248c3775bca789d1d81256b42cd6c1faec0
Mengwa Ugo <mengwa44@yahoo.com>
2023-03-06 22:33:02 +0100

    connected job to UserSchema

--- models/Job.js
+++ models/Job.js
@@ -19,7 +19,7 @@ const JobSchema = new mongoose.Schema({
         default: 'pending'
     },
     createdBy: {
-        type: Schema.Types.ObjectId,
+        type: mongoose.Types.ObjectId,
         ref: 'User',
         required: [true, 'please provide a user']
 
